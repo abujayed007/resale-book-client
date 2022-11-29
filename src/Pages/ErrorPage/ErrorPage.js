7f8caab0647832ef133b1e493717b2a6d28955ea
unknown <abujayed007@gmail.com>
2022-11-29 13:20:57 +0600

    error page completed

---
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='flex  w-1/3 mx-auto py-10'>
+            <div className=''>
+            <div className='text-center'>
+                <h1 className='text-2xl font-bold'>Ooops.....</h1>
+                <p className='font-bold'>File Not Found</p>
+                </div>
+                <img  className='h-[400px] rounded-full' src="https://cdn.windowsreport.com/wp-content/uploads/2017/08/ActiveX-error-429-in-Windows-10.jpg" alt='' />
+                
+            </div>
+
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/routes/routes/Routes.js
+++ src/routes/routes/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Books from "../../Pages/Books/Books";
+import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
@@ -30,6 +31,10 @@ const routes = createBrowserRouter([
            
         ]
     },
+    {
+        path:'*',
+        element:<ErrorPage></ErrorPage>
+    }
     
 ])
 
